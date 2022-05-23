3c97d6fb39afc10806c38da5a6a6778d64774bbd
AzrDream <1499325862@qq.com>
2022-05-23 13:05:29 +0800

    跳转到歌单详情界面

---
+++ src/views/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="detail">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Detail'
+}
+</script>
+
+<style scoped lang="scss">
+.detail{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #000;
+}
+</style>
