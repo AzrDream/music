cf46cdb3ddbfa908b854bfd3caba8d66ea808a96
AzrDream <1499325862@qq.com>
2022-05-23 17:03:07 +0800

    播放界面头部

---
+++ src/components/Player/PlayerHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="header">
+      <div class="header-left"></div>
+      <div class="header-title">
+        <h3>演员</h3>
+        <p>薛之谦</p>
+      </div>
+      <div class="header-right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerHeader',
+  data () {
+    return {
+      themes: ['theme', 'theme1', 'theme2'],
+      index: 0
+    }
+  },
+  props: {
+    // title: {
+    //   type: String,
+    //   default: '',
+    //   required: true
+    // }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../../assets/css/variable";
+  @import "../../assets/css/mixin";
+  .header{
+    width: 100%;
+    height: 100px;
+    @include bg_color();
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    z-index: 999;
+    .header-left,.header-right{
+      width: 84px;
+      height: 84px;
+      margin-top: 8px;
+    }
+    .header-left{
+      @include bg_img('../../assets/images/down');
+    }
+    .header-right{
+    }
+    .header-title {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      color: #fff;
+      font-weight: bold;
+      @include font_size($font_medium);
+      @include no-wrap();
+    }
+  }
+</style>
---
+++ src/views/Player.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="player">
+      <PlayerHeader></PlayerHeader>
+    </div>
+</template>
+
+<script>
+import PlayerHeader from '../components/Player/PlayerHeader'
+export default {
+  name: 'Player',
+  components: {
+    PlayerHeader
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.player{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: pink;
+}
+</style>
