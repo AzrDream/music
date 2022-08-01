d0c75277b2c9dfd9b02f7ea87db4e2f776cbeaa4
AzrDream <1499325862@qq.com>
2022-08-01 19:55:36 +0800

    个人中心展示

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ const Search = (resolve) => {
     resolve(module)
   })
 }
+const Account = (resolve) => {
+  import('../views/Account').then((module) => {
+    resolve(module)
+  })
+}
 
 Vue.use(VueRouter)
 
@@ -49,7 +54,8 @@ const routes = [
   },
   { path: '/singer', component: Singer },
   { path: '/rank', component: Rank },
-  { path: '/search', component: Search }
+  { path: '/search', component: Search },
+  { path: '/account', component: Account }
 ]
 
 const router = new VueRouter({
