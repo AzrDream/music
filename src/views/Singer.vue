1522970fb84df0b41c346c19bdac7eba6985e329
AzrDream <1499325862@qq.com>
2022-08-02 14:31:03 +0800

    歌手界面快捷导航

--- src/views/Singer.vue
+++ src/views/Singer.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="singer">
  <div class="singer-wrapper">
-   <ScrollView>
+   <ScrollView ref="scrollView">
      <ul class="list-wrapper">
-       <li class="list-group" v-for="(value, index) in list" :key="index">
+       <li class="list-group" v-for="(value, index) in list" :key="index" ref="group">
          <h2 class="group-title">{{keys[index]}}</h2>
          <ul>
            <li class="group-item" v-for="obj in list[index]" :key="obj.id">
@@ -14,6 +14,18 @@
        </li>
      </ul>
    </ScrollView>
+   <ul class="list-keys">
+     <li v-for="(key,index) in keys"
+         :key="key"
+         @touchstart.stop.prevent="touchstart"
+         @touchmove.stop.prevent="touchmove"
+         :class="{'active':currentIndex === index}">
+       {{key}}
+     </li>
+   </ul>
+   <div class="fix-title" v-show="fixTitle !== ''" ref="fixTitle">
+     {{fixTitle}}
+   </div>
  </div>
 </div>
 </template>
@@ -26,6 +38,38 @@ export default {
   components: {
     ScrollView
   },
+  methods: {
+    _keyDown (index) {
+      this.currentIndex = index
+      const offsetY = this.groupsTop[index]
+      this.$refs.scrollView.scrollTo(0, -offsetY)
+    },
+    touchstart (e) {
+      const index = parseInt(e.target.dataset.index)
+      this._keyDown(index)
+      this.beginOffsetY = e.touches[0].pageY
+    },
+    touchmove (e) {
+      this.moveOffsetY = e.touches[0].pageY
+      const offsetY = (this.moveOffsetY - this.beginOffsetY) / e.target.offsetHeight
+      let index = parseInt(e.target.dataset.index) + Math.floor(offsetY)
+      if (index < 0) {
+        index = 0
+      } else if (index > this.keys.length - 1) {
+        index = this.keys.length - 1
+      }
+      this._keyDown(index)
+    }
+  },
+  computed: {
+    fixTitle () {
+      if (this.scrollY >= 0) {
+        return ''
+      } else {
+        return this.keys[this.currentIndex]
+      }
+    }
+  },
   created () {
     getAllArtists()
       .then(result => {
@@ -36,10 +80,61 @@ export default {
         console.log(error)
       })
   },
+  mounted () {
+    this.$refs.scrollView.scrolling(y => {
+      this.scrollY = y
+      // 处理第一个区域
+      if (y > 0) {
+        this.currentIndex = 0
+        return
+      }
+      // 处理中间的区域
+      for (let i = 0; i < this.groupsTop.length - 1; i++) {
+        const preTop = this.groupsTop[i]
+        const nextTop = this.groupsTop[i + 1]
+        if (-y >= preTop && -y < nextTop) {
+          this.currentIndex = i
+          // 1.用下一组标题的偏移位 + 当前滚动出去的偏移位
+          const diffOffsetY = nextTop + y
+          let fixTitleOffsetY = 0
+          // 2.判断计算的结果是否是 0 ~ 分组标题高度的值
+          if (diffOffsetY >= 0 && diffOffsetY <= this.fixTitleHeight) {
+            // 满足条件开始移动上一组标题
+            fixTitleOffsetY = diffOffsetY - this.fixTitleHeight
+          } else {
+            // 不满足条件需要固定在顶部
+            fixTitleOffsetY = 0
+          }
+          if (fixTitleOffsetY === this.fixTitleOffsetY) {
+            return
+          }
+          this.fixTitleOffsetY = fixTitleOffsetY
+          this.$refs.fixTitle.style.transform = `translateY(${fixTitleOffsetY}px)`
+          return
+        }
+      }
+      // 处理最后一个区域
+      this.currentIndex = this.groupsTop.length - 1
+    })
+  },
   data () {
     return {
       keys: [],
-      list: []
+      list: [],
+      groupsTop: [],
+      currentIndex: 0,
+      beginOffsetY: 0,
+      moveOffsetY: 0,
+      scrollY: 0
+    }
+  },
+  watch: {
+    list () {
+      this.$nextTick(() => {
+        this.$refs.group.forEach(group => {
+          this.groupsTop.push(group.offsetTop)
+        })
+      })
     }
   }
 }
